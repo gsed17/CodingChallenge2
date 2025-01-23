053a124e2a74400e8c4977c7d7a3e51272276aa7
Garron Seddon <garronseddon@icloud.com>
2025-01-22 19:44:00 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -3,3 +3,9 @@ let products = ["Apples", "Bananas", "Oranges", "Grapes", "Mangoes"];
 products.push("Pineapples"); // Add a new product
 products.pop(); // Remove the last product
 console.log("Updated Products List:", products);
+// Task 2 - Student Scores
+let scores = [85, 90, 78, 92, 88];
+scores[1] = 95; // Update the second score
+const averageScore = scores.reduce((sum, score) => sum + score, 0) / scores.length;
+console.log("Updated Scores:", scores);
+console.log("Average Score:", averageScore);
