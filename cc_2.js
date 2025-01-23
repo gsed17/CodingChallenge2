5242b580eae9eaf269bc358cc8035b234674d957
Garron Seddon <garronseddon@icloud.com>
2025-01-22 19:43:34 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,5 @@
+// Task 1 - Store Inventory
+let products = ["Apples", "Bananas", "Oranges", "Grapes", "Mangoes"];
+products.push("Pineapples"); // Add a new product
+products.pop(); // Remove the last product
+console.log("Updated Products List:", products);
